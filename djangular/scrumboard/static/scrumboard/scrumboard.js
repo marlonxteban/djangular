f33774fd042ceb3913ebf394e181bc257ccfe941
marlonxteban <marlonxteban@gmail.com>
2017-10-18 13:27:47 -0500

    creando proyecto angular, actualizando repositorio

---
+++ djangular/scrumboard/static/scrumboard/scrumboard.js
@@ -0,0 +1,42 @@
+(function(){
+    'use strict';
+
+    angular.module('scrumboard.demo',[])
+        .controller('ScrumboardController', ['$scope',ScrumboardController]);
+    
+    function ScrumboardController($scope){
+        $scope.add = function (list, title){
+            var card = {
+                title: title
+            };
+            list.cards.push(card);
+        };
+         $scope.data = [
+             {
+                 name: 'Django demo',
+                 cards: [
+                     {
+                         title: 'Create Models'
+                     },
+                     {
+                         title: 'create View'
+                     },
+                     {
+                         title: 'Migrate Database'
+                     },
+                 ]
+             },
+             {
+                 name: 'Angular Demo',
+                 cards: [
+                     {
+                         title:'Write HTML'
+                     },
+                     {
+                         title: 'Write JavaScript'
+                     },
+                 ]
+             }
+         ];
+    }
+}());
